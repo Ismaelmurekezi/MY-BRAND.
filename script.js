dec0664a15fb39b70e40ae605397a7f292fef6d6
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-03-04 15:06:06 +0200

    login and signup

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburgerIcon=document.getElementById("hamburger-menu");
+const hamburgerIcon1=document.getElementById("hamburger-menu1");
+let navLinks=document.getElementById("nav-links");
+
+
+hamburgerIcon.addEventListener('click',()=>{
+    navLinks ?     navLinks.style.display='none':    menu.style.display='block'
+
+})
+
+
